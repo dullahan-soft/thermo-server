2d79e8286c0b42dae910c90b400f7c3a963c9cc5
Chris Cacciatore <chris.cacciatore@gmail.com>
2012-09-20 18:10:31 -0700

    Added schematic

--- dash/dash-server.rb
+++ dash/dash-server.rb
@@ -1,6 +1,15 @@
 require 'rubygems'
 require 'sinatra'
+require 'httparty'
 
 get '/' do
   send_file "dash.html"
+end
+
+get '/:args' do |args|
+  HTTParty.get("http://192.168.1.4/#{args}")
+end
+
+post '/pump/:state' do |state|
+    HTTParty.post("http://192.168.1.4/pump/#{state}",{})
 end
